d2dc0e246543dcb1e2880bd1de890c74126545a0
ponmanivasahan <vasupks01@gmail.com>
2025-11-17 11:40:29 +0530

    third  commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Routing from './pages/Routers/Routing'
-
-
-
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter  basename="/finalyoutube">
   <StrictMode>
     <Routing />
   </StrictMode>
